d1115b53e176d5c696c88c0b86104b7945b8dbbb
Prajwal <prajwal77dwivedi@gmail.com>
2025-11-15 23:38:50 +0530

    Initial Backend Setup

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "../config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch((error) => {
+    console.log(error);
+    process.exit(1);
+})
